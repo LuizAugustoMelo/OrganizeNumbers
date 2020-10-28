1cca55087bbfffccc4322cb472a8d5cb7f77a6ce
Luiz Augusto Melo <luiz@twistereventos.com>
2020-10-28 17:02:13 -0300

    Created Project. Created Organize and Exceptions Classes. Program completed.

---
+++ Entities/Organize.cs
@@ -0,0 +1,105 @@
+﻿using Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class Organize
+    {
+        public int Number { get; protected set; }
+
+        public Organize(int number)
+        {
+            if (number <= 0)
+            {
+                throw new OrganizeExceptions("O numero digitado deve ser maior que zero.");
+            }
+            Number = number;
+        }
+
+        public string MinorNumber()
+        {
+            string numReturn = "";
+            int index = -1, lowestValue = -1;
+            List<char> charNumber = new List<char>();
+            List<int> tempNum = new List<int>();
+
+            for (int i = 0; i < Number.ToString().Length; i++)
+            {
+                charNumber.Add(char.Parse(Number.ToString().Substring(i, 1)));
+            }
+
+            while(charNumber.Count > 0)
+            {
+                for (int i = 0; i < charNumber.Count; i++)
+                {
+                    int value = int.Parse(charNumber[i].ToString());
+
+                    if (i == 0)
+                    {
+                        lowestValue = value;
+                        index = i;
+                    }
+                    else if (value < lowestValue)
+                    {
+                        index = i;
+                        lowestValue = value;
+                    }
+                }
+
+                tempNum.Add(lowestValue);
+                charNumber.RemoveAt(index);
+            }
+
+            for (int i = 0; i < tempNum.Count; i++)
+            {
+                numReturn += tempNum[i].ToString();
+            }
+
+            return numReturn;
+        }
+
+        public string MajorNumber()
+        {
+            string numReturn = "";
+            int index = -1, highestValue = -1;
+            List<char> charNumber = new List<char>();
+            List<int> tempNum = new List<int>();
+
+            for (int i = 0; i < Number.ToString().Length; i++)
+            {
+                charNumber.Add(char.Parse(Number.ToString().Substring(i, 1)));
+            }
+
+            while (charNumber.Count > 0)
+            {
+                for (int i = 0; i < charNumber.Count; i++)
+                {
+                    int value = int.Parse(charNumber[i].ToString());
+
+                    if (i == 0)
+                    {
+                        highestValue = value;
+                        index = i;
+                    }
+                    else if (value > highestValue)
+                    {
+                        index = i;
+                        highestValue = value;
+                    }
+                }
+
+                tempNum.Add(highestValue);
+                charNumber.RemoveAt(index);
+            }
+
+            for (int i = 0; i < tempNum.Count; i++)
+            {
+                numReturn += tempNum[i].ToString();
+            }
+
+            return numReturn;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Entities;
+using Entities.Exceptions;
+using System;
+
+namespace OrganizeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para converter um numero inteiro para o maior ou menor possivél.");
+            try
+            {
+                Console.Write("Você deseja converter para Maior ou Menor (H/L)? ");
+                char typeConversion = char.Parse(Console.ReadLine().ToLower());
+
+                if (typeConversion != 'l' && typeConversion != 'h')
+                {
+                    throw new OrganizeExceptions("Tipo de seleção de conversão invalido.");
+                }
+
+                Console.Write("Entre com um numero inteiro: ");
+                Organize organizeNumbers = new Organize(int.Parse(Console.ReadLine().Trim()));
+                Console.WriteLine("Numero digitado: " + organizeNumbers.Number);
+
+                if (typeConversion == 'l')
+                {
+                    Console.WriteLine("Menor numero obtido: " + organizeNumbers.MinorNumber());
+                }
+                else if (typeConversion == 'h')
+                {
+                    Console.WriteLine("Maior numero obtido: " + organizeNumbers.MajorNumber());
+                }
+
+            }
+            catch (OrganizeExceptions e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
